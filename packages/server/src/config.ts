09d238c6746de64f822b5b6cde7d3e69ae8a7617
Diogo Neves <57763474+neves-nvs@users.noreply.github.com>
2024-08-05 13:30:32 +0100

    Add server config file

--- packages/server/src/config.ts
+++ packages/server/src/config.ts
@@ -2,6 +2,10 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const PORT = process.env.PORT || 8081;
+export const PORT = process.env.PORT || 8081;
 
-export { PORT };
+export const JWT_SECRET = process.env.JWT_SECRET;
+
+export const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID;
+export const GOOGLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET;
+
