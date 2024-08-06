f2421960fc70d0f935ee817c6230460e41f3af48
Diogo Neves <57763474+neves-nvs@users.noreply.github.com>
2024-08-07 00:27:47 +0100

    Update server linting config

--- packages/server/eslint.config.mjs
+++ packages/server/eslint.config.mjs
@@ -2,10 +2,14 @@ import globals from "globals";
 import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
 
-
+// 'src/**/*.{js,ts,tsx}'
 export default [
-  {files: ["**/*.{js,mjs,cjs,ts}"]},
-  {languageOptions: { globals: globals.browser }},
+  {
+    files: ["**/*.{js,mjs,cjs,ts}"],
+  },
+  {
+    languageOptions: { globals: globals.node },
+  },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
 ];
