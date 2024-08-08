5eb27c35212d7ba75d4f8104881b135e62c7f0d3
Diogo Neves <57763474+neves-nvs@users.noreply.github.com>
2024-08-08 23:30:39 +0100

    Add option to configure Morgan log format

--- packages/server/docs/Logging.md
+++ packages/server/docs/Logging.md
@@ -11,3 +11,16 @@ The project uses `winston` as the logger and `morgan`. The default log level is 
 5. `verbose`
 6. `debug`
 7. `silly`
+
+### Morgan
+
+`morgan` is the middleware used to log HTTP requests with `winston`. To change the log format, set the `HTTP_LOG_FORMAT` environment variable to one of the following values:
+
+1. `combined`
+2. `common`
+3. `dev`
+4. `short`
+5. `tiny`
+
+For more information, see the [morgan middleware documentation](https://expressjs.com/en/resources/middleware/morgan.html)
+
--- packages/server/src/morgan.ts
+++ packages/server/src/morgan.ts
@@ -1,8 +1,18 @@
 import morgan from "morgan";
 import logger from "./logger";
 import { Request } from "express";
+import dotenv from "dotenv";
 
-export const morganMiddleware = morgan("dev", {
+dotenv.config();
+
+const HTTP_LOG_FORMAT = process.env.LOG_FORMAT || "dev";
+if (process.env.HTTP_LOG_FORMAT) {
+  logger.info(`HTTP log format set to ${HTTP_LOG_FORMAT}`);
+} else {
+  logger.debug(`HTTP log format using default: ${HTTP_LOG_FORMAT}`);
+}
+
+export const morganMiddleware = morgan(HTTP_LOG_FORMAT, {
   stream: {
     write: (message) => logger.http(message.trim()),
   },
@@ -12,6 +22,6 @@ morgan.token("body", (req: Request) => JSON.stringify(req.body));
 
 export const morganBodyMiddleware = morgan("Request body: :body", {
   stream: {
-    write: (message) => logger.http(message.trim()),
+    write: (message) => logger.debug(message.trim()),
   },
 });
