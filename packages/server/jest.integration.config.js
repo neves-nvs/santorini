e993772a7438d68ff777b46b71b959ff06d11d8c
Diogo Neves <57763474+neves-nvs@users.noreply.github.com>
2024-08-23 15:23:22 +0100

    chore: Configure Jest integration tests for server-side code

---
+++ packages/server/jest.integration.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  preset: "ts-jest",
+  testEnvironment: "node",
+  testMatch: ["**/tests/integration/**/*.test.ts"],
+  globalSetup: "<rootDir>/tests/integration/globalSetup.ts",
+  globalTeardown: "<rootDir>/tests/integration/globalTeardown.ts",
+  verbose: true,
+  moduleNameMapper: {
+    "@src/(.*)": "<rootDir>/src/$1",
+  },
+};
