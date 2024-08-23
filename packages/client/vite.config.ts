8debedafe47a9459add7facc0b94ee9dbd70795e
Diogo Neves <57763474+neves-nvs@users.noreply.github.com>
2024-08-23 14:08:49 +0100

    chore: Add vite.config.ts for resolving aliases in the client code

---
+++ packages/client/vite.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from "vite";
+
+import path from "path";
+export default defineConfig({
+  resolve: {
+    alias: {
+      "@src": path.resolve(__dirname, "./src"),
+      "@services": path.resolve(__dirname, "./src/services"),
+    },
+  },
+});
