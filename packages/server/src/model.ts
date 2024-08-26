fae6a64b25df7e6fa7a23144064a670b6acfdaba
Diogo Neves <57763474+neves-nvs@users.noreply.github.com>
2024-08-26 23:26:50 +0100

    refactor: Update UserTable model to include password_hash and password_salt properties

--- packages/server/src/model.ts
+++ packages/server/src/model.ts
@@ -1,10 +1,4 @@
-import {
-  ColumnType,
-  Generated,
-  Insertable,
-  Selectable,
-  Updateable,
-} from "kysely";
+import { ColumnType, Generated, Insertable, Selectable, Updateable } from "kysely";
 
 export interface Database {
   users: UserTable;
