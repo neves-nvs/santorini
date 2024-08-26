71eea3640aef671b18958081d5aa6d8aeba14de9
Diogo Neves <57763474+neves-nvs@users.noreply.github.com>
2024-08-26 23:18:26 +0100

    refactor: Update logger format to include timestamp before log level

--- packages/server/src/logger.ts
+++ packages/server/src/logger.ts
@@ -1,5 +1,6 @@
 import dotenv from "dotenv";
 import { createLogger, format, transports } from "winston";
+import { FORCE_LOGS } from "./config";
 
 const { combine, timestamp, colorize, printf } = format;
 
@@ -10,7 +11,7 @@ const uppercaseLevel = format((info) => {
 });
 
 const logFormat = printf(({ level, message, timestamp, ...metadata }) => {
-  let msg = `[${level}] ${timestamp} - ${message}`;
+  let msg = `${timestamp} [${level}]  ${message}`;
   if (metadata && Object.keys(metadata).length) {
     msg += ` | ${JSON.stringify(metadata)}`;
   }
@@ -22,12 +23,17 @@ dotenv.config();
 const DEFAULT_LOG_LEVEL = "http";
 const LOG_LEVEL = process.env.LOG_LEVEL ?? DEFAULT_LOG_LEVEL;
 
+let silence = process.env.NODE_ENV === "test";
+if (FORCE_LOGS === true) {
+  silence = false;
+}
+
 const logger = createLogger({
   level: LOG_LEVEL,
   format: combine(uppercaseLevel(), colorize(), timestamp({ format: "HH:mm:ss" }), logFormat),
   transports: [
     new transports.Console({
-      silent: process.env.NODE_ENV === "test",
+      silent: silence,
     }),
   ],
 });
@@ -37,4 +43,9 @@ if (process.env.LOG_LEVEL) {
 } else {
   logger.info(`Log level using default: '${LOG_LEVEL}'`);
 }
+
+if (FORCE_LOGS === true) {
+  logger.warn("Forcing logs");
+}
+
 export default logger;
