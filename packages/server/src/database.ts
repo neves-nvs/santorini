8a725406eb401bc81d6a5620e2fc232f6a8360c4
Diogo Neves <57763474+neves-nvs@users.noreply.github.com>
2024-08-26 23:29:22 +0100

    refactor: Update database configuration to use test database

--- packages/server/src/database.ts
+++ packages/server/src/database.ts
@@ -1,18 +1,22 @@
 import { Pool } from "pg";
 import { Kysely, PostgresDialect } from "kysely";
-import { Database } from "@src/model";
+import { Database } from "../src/model";
 import { DB_PORT } from "./config";
+import logger from "./logger";
 
 const PORT = DB_PORT ?? 5432;
 
-const dialect = new PostgresDialect({
-  pool: new Pool({
-    database: "auth",
-    host: "localhost",
-    user: "postgres",
-    password: "postgres",
-    port: PORT,
-  }),
+const POOL_CONFIG = {
+  database: "test",
+  host: "localhost",
+  user: "test",
+  password: "test",
+  port: PORT,
+};
+logger.info("POOL_CONFIG", POOL_CONFIG);
+
+export const dialect = new PostgresDialect({
+  pool: new Pool(POOL_CONFIG),
 });
 
 export const db = new Kysely<Database>({
