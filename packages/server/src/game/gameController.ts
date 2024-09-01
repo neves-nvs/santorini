714f9a8d6049ffc89023355e048ba95c63187fd5
Diogo Neves <57763474+neves-nvs@users.noreply.github.com>
2024-09-01 18:32:16 +0100

    refactor: Update gameController to use authentication middleware and validate amountOfPlayers parameter

--- packages/server/src/game/gameController.ts
+++ packages/server/src/game/gameController.ts
@@ -1,204 +1,102 @@
-import { Router } from "express";
-import { body } from "express-validator";
-import logger from "../logger";
-import {
-  checkValidation,
-  deprecate,
-  validateUUIDParam,
-} from "../utils/middleware";
-import { findUserByUsername } from "../users/userRepository";
 import * as gameRepository from "./gameRepository";
+import * as gameService from "./gameService";
+
+import { checkValidation, validateUUIDParam } from "../middlewares/middleware";
+
+import { Router } from "express";
+import { User } from "../model";
+import { authenticate } from "../auth/authController";
+import { body } from "express-validator";
 
 const router = Router();
 
-router.get("/", async (req, res) => {
-  res.send(await gameRepository.getAllGameIds());
+/* -------------------------------------------------------------------------- */
+/*                                      /                                     */
+/* -------------------------------------------------------------------------- */
+
+router.get("/", authenticate, async (req, res) => {
+  res.send(await gameRepository.getAllGames());
 });
 
 router.post(
   "/",
-  body("username").isString().notEmpty().withMessage("Username is required"),
-  body("amountOfPlayers")
-    .isInt({ min: 2, max: 4 })
-    .withMessage("Amount of players must be between 2 and 4"),
-  async (req, res) => {
-    const { username, amountOfPlayers } = req.body as {
-      username: string;
-      amountOfPlayers: number | undefined;
-    };
-    const game = await gameRepository.createGame({ username, amountOfPlayers });
-    res.status(201).send({ gameId: game.getId() });
-  },
-);
-
-router.post(
-  "/:gameId/players",
-  deprecate,
-  validateUUIDParam("gameId"),
+  authenticate,
+  body("amountOfPlayers").isInt({ min: 2, max: 4 }).withMessage("Amount of players must be between 2 and 4"),
   checkValidation,
-  body("username").notEmpty().withMessage("Username is required"),
-  async (req, res) => {
-    const { gameId } = req.params;
-    const { username } = req.body as { username: string };
-    const game = await gameRepository.findGameById(gameId);
-    if (!game) {
-      return res.status(400).send("Game not found");
-    }
-    const user = await findUserByUsername(username);
-    if (!user) {
-      return res.status(400).send("User not found");
-    }
+  async (req, res, next) => {
+    const { amountOfPlayers } = req.body as { amountOfPlayers: number | undefined };
+    const user = req.user as User;
 
-    game.addPlayer(user);
-    res.status(201).send();
-  },
-);
-
-router.post(
-  "/join",
-  deprecate,
-  body("username").notEmpty().withMessage("Username is required"),
-  body("gameID").isUUID().withMessage("Game ID must be a valid UUID"),
-  checkValidation,
-  async (req, res) => {
-    logger.info("POST /games/join", req.body);
-    const { username, gameID } = req.body;
-    if (!gameID) {
-      return res.status(400).send("Game ID required");
-    }
-    if (!username) {
-      return res.status(400).send("Username required");
-    }
-
-    const game = await gameRepository.findGameById(gameID);
-    if (!game) {
-      return res.status(400).send("Game not found");
-    }
-    const user = findUserByUsername(username);
-    if (!user) {
-      return res.status(400).send("User not found");
-    }
-
-    // const success = game.addPlayer(user);
-    // if (!success) {
-    //   return res.status(400).send("Game full");
-    // }
-
-    if (game.isReadyToStart()) {
-      game.start();
-      const currentPlayer = game.getCurrentPlayer();
-      console.assert(currentPlayer, "No current player");
-      if (!currentPlayer) {
-        return res.status(500).send("No current player");
+    try {
+      const result = await gameService.createGame(user, amountOfPlayers);
+      res.status(201).send(result);
+    } catch (error) {
+      if (error instanceof Error && error.message === "Failed to create game") {
+        return res.status(400).send({ message: "Failed to create game" });
       }
-      game.updatePlays(currentPlayer.username);
+      next(error);
     }
-
-    res.status(201).send({ gameId: game.getId() });
   },
 );
 
-router.get(
-  "/:gameId/plays",
-  validateUUIDParam("gameId"),
-  checkValidation,
-  async (req, res) => {
-    const { gameId } = req.params;
-    // TODO should be available from the session
-    const { playerId } = req.body;
-    if (!playerId) {
-      return res.status(400).send("Player ID required");
-    }
-    const player = await findUserByUsername(playerId);
-    if (!player) {
-      return res.status(400).send("Player not found");
-    }
-    const username = player.username;
+/* -------------------------------------------------------------------------- */
+/*                              /:gameId/players                              */
+/* -------------------------------------------------------------------------- */
 
-    const game = await gameRepository.findGameById(gameId);
-    if (!game) {
-      return res.status(400).send("Game not found");
-    }
+router.post("/:gameId/players", authenticate, validateUUIDParam("gameId"), checkValidation, async (req, res, next) => {
+  const gameId = parseInt(req.params.gameId);
+  const user = req.user as User;
 
-    res.send(game.getPlays(username));
-  },
-);
-
-router.get(
-  "/plays",
-  deprecate,
-  body("gameID").isString().notEmpty().withMessage("Game ID required"),
-  body("playerID").isString().notEmpty().withMessage("Player ID required"),
-  checkValidation,
-  async (req, res) => {
-    logger.info("GET /games/plays", req.body);
-    const { gameID, playerID } = req.body;
-    const game = await gameRepository.findGameById(gameID);
-    if (!game) {
-      return res.status(400).send("Game not found");
-    }
-    const player = await findUserByUsername(playerID);
-    if (!player) {
-      return res.status(400).send("Player not found");
-    }
-
-    const moves = game.getPlays(player.username);
-    res.send(moves);
-  },
-);
-
-router.post(
-  "/:gameId/plays",
-  validateUUIDParam("gameId"),
-  body("playerID").isString().notEmpty().withMessage("Player ID required"),
-  checkValidation,
-  async (req, res) => {
-    const { gameId } = req.params;
-
-    // TODO should be available from the session
-    const { playerID, play } = req.body;
-    const player = await findUserByUsername(playerID);
-    if (!player) {
-      return res.status(400).send("Player not found");
-    }
-
-    if (!play) {
-      return res.status(400).send("Play required");
-    }
-
-    const game = await gameRepository.findGameById(gameId);
-    if (!game) {
-      return res.status(400).send("Game not found");
-    }
-
-    return res.status(501).send("Post plays not implemented");
-    // game.addPlay(player.getUsername(), play);
-    // res.status(201).send();
-  },
-);
-
-router.post(
-  "/plays",
-  deprecate,
-  body("gameID").isString().notEmpty().withMessage("Game ID required"),
-  body("playerID").isString().notEmpty().withMessage("Player ID required"),
-  body("play").isString().notEmpty().withMessage("Play required"),
-  async (req, res) => {
-    logger.info("POST /games/plays", req.body);
-    const { gameID, playerID } = req.body;
-    // const play = req.body.play;
-
-    const game = await gameRepository.findGameById(gameID);
-    if (!game) {
-      return res.status(400).send("Game not found");
-    }
-    const player = await findUserByUsername(playerID);
-    if (!player) {
-      return res.status(400).send("Player not found");
-    }
-
+  try {
+    await gameService.addPlayerToGame(gameId, user);
     res.status(201).send();
-  },
-);
+  } catch (error) {
+    if (error instanceof Error && error.message === "Game not found") {
+      return res.status(400).send("Game not found");
+    }
+    next(error);
+  }
+});
+
+/* -------------------------------------------------------------------------- */
+/*                               /:gameId/plays                               */
+/* -------------------------------------------------------------------------- */
+
+router.get("/:gameId/plays", authenticate, validateUUIDParam("gameId"), checkValidation, async (req, res) => {
+  const gameId = parseInt(req.params.gameId);
+
+  const user = req.user as User;
+
+  const game = await gameRepository.findGameById(gameId);
+  if (!game) {
+    return res.status(400).send("Game not found");
+  }
+
+  // res.send(gameService.getPlays(user.username));
+
+  res.status(501).send("Get plays not implemented");
+});
+
+router.post("/:gameId/plays", authenticate, validateUUIDParam("gameId"), checkValidation, async (req, res) => {
+  const gameId = parseInt(req.params.gameId);
+
+  const user = req.user as User;
+
+  const { play } = req.body;
+  if (!play) {
+    return res.status(400).send("Play required");
+  }
+
+  const game = await gameRepository.findGameById(gameId);
+  if (!game) {
+    return res.status(400).send("Game not found");
+  }
+
+  // gameService.applyPlay(gameId, user.username, play);
+
+  return res.status(501).send("Post plays not implemented");
+  // game.addPlay(player.getUsername(), play);
+  // res.status(201).send();
+});
 
 export default router;
