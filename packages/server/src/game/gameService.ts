fb7c25adc605aa4496a12af9ec747f8ab6d3a005
Diogo Neves <57763474+neves-nvs@users.noreply.github.com>
2024-09-08 22:24:44 +0200

    feat: Update gameService to support transactional queries

--- packages/server/src/game/gameService.ts
+++ packages/server/src/game/gameService.ts
@@ -2,6 +2,8 @@ import * as gameRepository from "./gameRepository";
 
 import { NewGame, User } from "../model";
 import { broadcastUpdate } from "./gameSession";
+import { db } from "../database";
+import logger from "../logger";
 
 export async function createGame(user: User, amountOfPlayers: number = 2): Promise<{ gameId: number }> {
   const newGame = {
@@ -18,21 +20,26 @@ export async function createGame(user: User, amountOfPlayers: number = 2): Promi
 }
 
 export async function addPlayerToGame(gameId: number, user: User): Promise<void> {
-  const game = await gameRepository.findGameById(gameId);
-  if (!game) {
-    throw new Error("Game not found");
-  }
+  await db
+    .transaction()
+    .setIsolationLevel("serializable")
+    .execute(async (transaction) => {
+      const game = await gameRepository.findGameById(gameId, transaction);
+      if (!game) {
+        throw new Error("Game not found");
+      }
 
-  // TODO move to DB constraint
-  const playersInGame = await gameRepository.findPlayersByGameId(gameId);
-  if (game.player_count >= playersInGame.length) {
-    throw new Error("Game is full");
-  }
+      const playersInGame = await gameRepository.findPlayersByGameId(gameId, transaction);
+      if (playersInGame.length >= game.player_count) {
+        logger.error("Game is full");
+        throw new Error("Game is full");
+      }
 
-  await gameRepository.addPlayerToGame(gameId, user.id);
+      await gameRepository.addPlayerToGame(gameId, user.id, transaction);
 
-  const playersCount = (await gameRepository.findPlayersByGameId(gameId)).length;
-  if (playersCount === game.player_count) {
-    broadcastUpdate(gameId, { type: "game_start" });
-  }
+      const playersCount = (await gameRepository.findPlayersByGameId(gameId, transaction)).length;
+      if (playersCount === game.player_count) {
+        broadcastUpdate(gameId, { type: "game_start" });
+      }
+    });
 }
