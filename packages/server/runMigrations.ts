d8a79c119c2b38bfb78639fe4a58cb46daaae4ba
Diogo Neves <57763474+neves-nvs@users.noreply.github.com>
2025-07-22 23:48:17 +0100

    save state

---
+++ packages/server/runMigrations.ts
@@ -0,0 +1,35 @@
+import { FileMigrationProvider, Migrator } from "kysely";
+import { db } from "./src/database";
+import { promises as fs } from "fs";
+import path from "path";
+
+async function migrateToLatest() {
+  const migrator = new Migrator({
+    db,
+    provider: new FileMigrationProvider({
+      fs,
+      path,
+      migrationFolder: path.join(__dirname, "./migrations"),
+    }),
+  });
+
+  const { error, results } = await migrator.migrateToLatest();
+
+  results?.forEach((it) => {
+    if (it.status === "Success") {
+      console.log(`migration "${it.migrationName}" was executed successfully`);
+    } else if (it.status === "Error") {
+      console.error(`failed to execute migration "${it.migrationName}"`);
+    }
+  });
+
+  if (error) {
+    console.error("failed to migrate");
+    console.error(error);
+    process.exit(1);
+  }
+
+  await db.destroy();
+}
+
+migrateToLatest();
---
+++ packages/server/src/board/constants.ts
@@ -0,0 +1,4 @@
+export const MAX_BASE_COUNT = 22;
+export const MAX_MID_COUNT = 18;
+export const MAX_TOP_COUNT = 14;
+export const MAX_DOME_COUNT = 18;
